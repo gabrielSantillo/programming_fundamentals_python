0b327c2f95e478378533634ee6d70088d5d5b9ef
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-10 10:32:36 -0700

    Data types

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 # print("Hello World")
 
-print('   /|')
-print('  / |')
-print(' /  |')
-print('/___|')
+# print('   /|')
+# print('  / |')
+# print(' /  |')
+# print('/___|')
+
+character_name = "Gabriel"
+character_age = 26
+is_male = True
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
