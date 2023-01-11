2c26f76a4551d256569251533fc1aafe1aacbdf7
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-11 13:46:37 -0700

    Object functions

--- app.py
+++ app.py
@@ -210,26 +210,34 @@
 
 # print(student_one.scores[0])
 
-from question import Question
+# from question import Question
 
-question_prompts = [
-    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
-    "What colors are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
-    "What color are strwberries?\n(a) Teal\n(b) Red\n(c) Blue\n\n"
-]
+# question_prompts = [
+#     "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+#     "What colors are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+#     "What color are strwberries?\n(a) Teal\n(b) Red\n(c) Blue\n\n"
+# ]
 
-questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "c"),
-    Question(question_prompts[2], "b")
-]
+# questions = [
+#     Question(question_prompts[0], "a"),
+#     Question(question_prompts[1], "c"),
+#     Question(question_prompts[2], "b")
+# ]
 
-def run_test(questions):
-    score = 0
-    for question in questions:
-        answer = input(question.prompt)
-        if answer == question.answer:
-            score += 1
-    print("You got " + str(score) + "/" + str(len(questions)) + " out of questions.")
+# def run_test(questions):
+#     score = 0
+#     for question in questions:
+#         answer = input(question.prompt)
+#         if answer == question.answer:
+#             score += 1
+#     print("You got " + str(score) + "/" + str(len(questions)) + " out of questions.")
 
-run_test(questions)
+# run_test(questions)
+
+from student import Student
+
+student1 = Student("Oscar", "Accouting", 3.8, False, 80)
+student2 = Student("Jim", "Business", 3.4, False, 60)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
--- student.py
+++ student.py
@@ -5,3 +5,9 @@ class Student:
         self.gpa = gpa
         self.is_on_probation = is_on_probation
         self.scores = scores
+
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
