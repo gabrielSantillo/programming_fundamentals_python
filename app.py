f0bc188e8baeb0680404c7863926573bf98d1ec9
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-10 18:20:14 -0700

    guessing game

--- app.py
+++ app.py
@@ -117,9 +117,27 @@
 #     "Dec": "December"
 # }
 
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
 
-print("Done with loop")
+# print("Done with loop")
+
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Out of guesses, YOU LOSE")
+else:
+    print("You win!!!")
