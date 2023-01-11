9939b2bcb21b9fb51e54e5c1d49c317ac737d99c
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-11 10:03:55 -0700

    Adding as err to the excep clause

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ try:
     value = 10/0
     number = int(input("Enter a number: "))
     print(number )
-except ZeroDivisionError:
-    print("You can't divide by zero")
+except ZeroDivisionError as err:
+    print(err)
 except ValueError:
     print("Invalid input")
