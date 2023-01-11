8c2c7bb1646ecab2175c3e8d1686961e6501f825
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-11 10:01:39 -0700

    Try except clauses

--- app.py
+++ app.py
@@ -167,16 +167,25 @@
 #         print(col)
 
 
-def translate(phrase):
-    translation = ""
-    for letter in phrase:
-        if letter.lower() in "aeiou":
-            if letter.isupper():
-                translation += "G"
-            else:
-                translation += "g"   
-        else:
-            translation += letter
-    return translation
+# def translate(phrase):
+#     translation = ""
+#     for letter in phrase:
+#         if letter.lower() in "aeiou":
+#             if letter.isupper():
+#                 translation += "G"
+#             else:
+#                 translation += "g"   
+#         else:
+#             translation += letter
+#     return translation
 
-print(translate(input("Enter a phrase: \n")))
+# print(translate(input("Enter a phrase: \n")))
+
+try:
+    value = 10/0
+    number = int(input("Enter a number: "))
+    print(number )
+except ZeroDivisionError:
+    print("You can't divide by zero")
+except ValueError:
+    print("Invalid input")
