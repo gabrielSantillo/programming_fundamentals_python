9a3cb7c906ceb1fef98816c6c7a473be3f01328b
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-10 17:58:47 -0700

    Dictionaries

--- app.py
+++ app.py
@@ -87,17 +87,32 @@
 #     print("You are not a male nor tall")
 
 
-num1 = float(input("Enter first number: "))
-op = input("Enter operator: ")
-num2 = float(input("Enter first number: "))
+# num1 = float(input("Enter first number: "))
+# op = input("Enter operator: ")
+# num2 = float(input("Enter first number: "))
 
-if op == "+":
-    print(num1 + num2)
-elif op == "-":
-    print(num1 - num2)
-elif op == "/":
-    print(num1 / num2)
-elif op == "*":
-    print(num1 * num2)
-else:
-    print("Invalid operator")
+# if op == "+":
+#     print(num1 + num2)
+# elif op == "-":
+#     print(num1 - num2)
+# elif op == "/":
+#     print(num1 / num2)
+# elif op == "*":
+#     print(num1 * num2)
+# else:
+#     print("Invalid operator")
+
+month_conversations = {
+    "Jan": "Janury",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
