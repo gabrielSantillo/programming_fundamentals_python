71151aee1b534c5fc5fc7316fea8362263932510
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-11 13:37:53 -0700

    Multiple Quiz test

--- app.py
+++ app.py
@@ -200,12 +200,36 @@
 
 # https://docs.python.org/3/py-modindex.html
 
-from student import Student
+# from student import Student
 
-student_one = Student("Gabriel", "Computer Science", "4.8", False, [10, 7, 6])
+# student_one = Student("Gabriel", "Computer Science", "4.8", False, [10, 7, 6])
 
-student = {
-    "name": "GABRIEL"
-}
+# student = {
+#     "name": "GABRIEL"
+# }
 
-print(student_one.scores[0])
+# print(student_one.scores[0])
+
+from question import Question
+
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What colors are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strwberries?\n(a) Teal\n(b) Red\n(c) Blue\n\n"
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " out of questions.")
+
+run_test(questions)
