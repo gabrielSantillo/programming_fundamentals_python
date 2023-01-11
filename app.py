66e02caa1d777c640089db42cc44d4045bc68ad3
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-10 18:54:22 -0700

    2D lists and nested for loops

--- app.py
+++ app.py
@@ -149,8 +149,19 @@
 #     print(friends[index])
 
 
-def raise_to_power(base_num, pow_num):
-    result = pow(base_num, pow_num)
-    return result
+# def raise_to_power(base_num, pow_num):
+#     result = pow(base_num, pow_num)
+#     return result
 
-print(raise_to_power(2, 3))
+# print(raise_to_power(2, 3))
+
+
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for row in number_grid:
+    for col in row:
+        print(col)
