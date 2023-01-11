74e7c05521c87907b33b95266baaebda73c8fffb
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-10 19:06:32 -0700

    Just adding more efficient code

--- app.py
+++ app.py
@@ -170,8 +170,11 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation += "g"
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation += "G"
+            else:
+                translation += "g"   
         else:
             translation += letter
     return translation
